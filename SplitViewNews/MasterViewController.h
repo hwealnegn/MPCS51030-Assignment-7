3a4515343d945d39b05778894f1f825d773d5dc6
Helen Wang <helenwang@jolteon.cs.uchicago.edu>
2015-02-13 15:22:47 -0600

    Initial Commit

---
+++ SplitViewNews/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  SplitViewNews
+//
+//  Created by Helen Wang on 2/13/15.
+//  Copyright (c) 2015 Helen Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ SplitViewNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SplitViewNews
+//
+//  Created by Helen Wang on 2/13/15.
+//  Copyright (c) 2015 Helen Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
